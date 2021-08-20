a412c4066fa6792c4b298de833f8c0ce16d1a483
igorFachini <igor.fachini@catolicasc.org.br>
2021-08-20 18:16:49 -0300

    input range

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,5 +1,12 @@
 <template>
   <q-page class="flex flex-center">
+    <v-input
+      v-model="modelData"
+      :label="$t('date')"
+      date
+      range
+      @update:model-value="t"
+    />
     <img
       alt="Quasar logo"
       src="~assets/quasar-logo-vertical.svg"
@@ -12,6 +19,27 @@
 import { defineComponent } from 'vue'
 
 export default defineComponent({
-  name: 'PageIndex'
+  name: 'PageIndex',
+
+  data () {
+    return {
+      modelData: {
+        from: '',
+        to: ''
+      }
+    }
+  },
+
+  watch: {
+    modelData (val) {
+      console.log('val', val)
+    }
+  },
+
+  methods: {
+    t (t) {
+      console.log('t', t)
+    }
+  }
 })
 </script>
